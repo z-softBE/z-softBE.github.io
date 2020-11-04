0a7461b51e13852e146cc1eb8daf112fdbde023b
Wout <schoovaertswout@live.be>
2020-11-04 18:38:54 +0100

    Fixed mobile navigation

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -51,12 +51,13 @@ $(document).ready(() => {
             class: 'mobile-nav d-lg-none'
         });
         $('body').append($mobile_nav);
-        $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+        $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="fas fa-bars"></i></button>');
         $('body').append('<div class="mobile-nav-overly"></div>');
 
         $(document).on('click', '.mobile-nav-toggle', function(e) {
             $('body').toggleClass('mobile-nav-active');
-            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            $('.mobile-nav-toggle i').toggleClass('fa-times');
+            $('.mobile-nav-toggle i').toggleClass('fa-bars');
             $('.mobile-nav-overly').toggle();
         });
 
@@ -71,7 +72,8 @@ $(document).ready(() => {
             if (!container.is(e.target) && container.has(e.target).length === 0) {
                 if ($('body').hasClass('mobile-nav-active')) {
                     $('body').removeClass('mobile-nav-active');
-                    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+                    $('.mobile-nav-toggle i').toggleClass('fa-times');
+                    $('.mobile-nav-toggle i').toggleClass('fa-bars');
                     $('.mobile-nav-overly').fadeOut();
                 }
             }
